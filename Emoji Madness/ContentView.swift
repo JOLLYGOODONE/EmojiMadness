da57f40147eaf7ff4f2fd9f15facd749c5f2da9f
Brian Valentine <brianvalentine.networkengineer@gmail.com>
2025-04-17 19:49:13 -0500

    Initial Commit

---
+++ Emoji Madness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emoji Madness
+//
+//  Created by BRIAN VALENTINE on 4/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Emoji Madness/Emoji_MadnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_MadnessApp.swift
+//  Emoji Madness
+//
+//  Created by BRIAN VALENTINE on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_MadnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
